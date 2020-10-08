4da76554b31e4fec489a8b1ce9177c45b06c6ddb
Prashant Chaturvedi <prashant.nixsys@outlook.com>
2020-10-08 22:11:10 +0530

    Solves Problem 2 from Project Euler

---
+++ src/main/java/org/brokencodes/learning/dap/core/commons/FibonacciSequenceGenerator.java
@@ -0,0 +1,21 @@
+package org.brokencodes.learning.dap.core.commons;
+
+import java.util.function.LongSupplier;
+
+/**
+ * Generates a stream of Fibonacci sequences.
+ */
+public class FibonacciSequenceGenerator implements LongSupplier {
+
+    long current = 1;
+    long previous = 0;
+
+    @Override
+    public long getAsLong() {
+        long result = current;
+        current += previous;
+        previous = result;
+        return result;
+    }
+
+}
