c67e57f1bb503450556d2b390db75cfea1ca5bb3
Prashant Chaturvedi <prashant.nixsys@outlook.com>
2020-10-10 11:49:10 +0530

    Fixes javadocs

--- src/main/java/org/brokencodes/learning/dap/core/problems/projecteuler/ProjectEulerP1.java
+++ src/main/java/org/brokencodes/learning/dap/core/problems/projecteuler/ProjectEulerP1.java
@@ -6,7 +6,7 @@ import org.brokencodes.learning.dap.core.problems.ISolve;
 import java.util.stream.IntStream;
 
 /**
- * <h1>Multiples of 3 and 5</h1>
+ * <p>Multiples of 3 and 5</p>
  * <p>
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these
  * multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
